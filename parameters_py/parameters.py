ef10c8539a0ed2e6ea0c045c25fc30e0b65723df
Tinoet <88379020+Tinoet@users.noreply.github.com>
2022-03-29 20:52:25 +0100

    Add files via upload

---
+++ parameters_py/parameters.py
@@ -0,0 +1,10 @@
+# add two line breaks when a funtion is defined
+def greet_user(first_name, last_name):
+  print(f'hi {first_name},{last_name}')
+  print('welcome aboard')
+
+
+print('start')
+greet_user ('Elijah', 'Tochi')
+print('finish')
+# argument is the vale we apply to a function
